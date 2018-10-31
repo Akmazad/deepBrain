4eee9b8ada17eb4dab06821be049d2e321219019
Akmazad <a.azad@monash.edu>
2018-10-31 16:02:38 +1100

    Create windowMat_new.r

---
+++ windowMat_new.r
@@ -0,0 +1,41 @@
+windowMatForAchr <- function(datadir, binSize){
+    ## Load libraries
+    library('RCurl')
+    library('derfinder')
+    library('GenomicRanges')
+    
+    ## first get the bam file paths
+    bam_files <- list.files(path = datadir, pattern="*.sorted.bam$", recursive=TRUE, full.names=TRUE)
+    ##  then make the bai paths since they are meant to be in the same directory here as the bam file
+    bai_files <- paste(bam_files, ".bai", sep="")
+    
+    ## Load the data from disk -- for choromose 2 for example
+    ## allChrs = c('1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', 'X', 'Y', 'MT')
+    allChrs = c('1', 'X', 'Y', 'MT')
+    fullCov = fullCoverage(files = bam_files, bam = bai_files, chrs = allChrs)
+
+
+    ## summed coverage for each bin for all the samples (bam)
+    out = NULL
+    for(chrID in 1:25){
+        start <- 1
+        b <- binSize
+        binID <- 1
+        chrLength <- as.numeric(sapply(fullCov[chrID], nrow))
+        print(chrLength)
+        message(paste("Chromosome ",allChrs[chrID]," has started","\n"), appendLF=FALSE)
+        message(paste("bins done: ",binID,", "), appendLF=FALSE)
+        while (start < chrLength){
+            end <- if(start + b - 1 <= chrLength) (start + b - 1) else chrLength 
+            bin <- window(fullCov[chrID], start, end)
+            sb <- sapply(bin,sum)
+            names(sb) <- paste(names(fullCov[chrID]), "_", binID)
+            out <- rbind(out,sb)
+            start <- end + 1
+            binID <- binID + 1
+          }
+          message(paste("Chromosome ",allChrs[chrID]," has been completed","\n"), appendLF=FALSE)
+      }
+      write.csv(out,file = "outputMatForAll.csv")
+}
+
