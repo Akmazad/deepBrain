5dd1b16ef7acd91e74e4724ff5cf938e4c90c519
Akmazad <a.azad@monash.edu>
2018-11-17 11:55:38 +1100

    Update and rename accetylation2.r to accetylationDat2.r

--- accetylationDat2.r
+++ accetylationDat2.r
@@ -15,79 +15,92 @@ outputPath = "C:\\Users\\Azad\\OneDrive - UNSW\\Vafaee Lab\\Projects\\Deep Brain
 accetylationDat <- function(ba9_81.filepath, ba41_66.filepath, baVermis_62.filepath, samplefilePath, chrFile, binSize, overlapCutoff, outputPath){
   library("reproducible")
   ### load files
-  ba9_81.dat <- read.csv(ba9_81.filepath,header = TRUE)
-  ba41_66.dat <- read.csv(ba41_66.filepath,header = TRUE)
-  baVermis.dat <- read.csv(baVermis_62.filepath,header = TRUE)
-  sample.dat <- read.csv(samplefilePath,header = TRUE,stringsAsFactors = FALSE)
-  
-  ## make header (includes sampleID composed as brainID.RegionID)
-  header <- c("ChrID", "start", "end", paste0(sample.dat$BrainID,".",sample.dat$RegionID))
+  ba9_81.dat <- read.csv(ba9_81.filepath,header = TRUE, stringsAsFactors = FALSE)
+  ba41_66.dat <- read.csv(ba41_66.filepath,header = TRUE, stringsAsFactors = FALSE)
+  baVermis.dat <- read.csv(baVermis_62.filepath,header = TRUE, stringsAsFactors = FALSE)
+  sample.dat <- read.csv(samplefilePath,header = TRUE, stringsAsFactors = FALSE)
   
   ## read chromosome length file
   chrInfo = read.csv(file=chrFile, sep="\t", stringsAsFactors = FALSE)
   chrInd <- 1
   out <- NULL
+  
   while(chrInd <=1 ){
     chrID <- toString(chrInfo$chrID[chrInd])
     chrLength <- chrInfo$chr_length[chrInd]
     wStart <- 12900
     binID <- 1
-    #while (wStart < chrLength){
+    #while (wStart < chrLength){  
     while (wStart < 13200){
       wEnd <- if(wStart + binSize - 1 <= chrLength) (wStart + binSize - 1) else chrLength
-      ## read data within a window of a particular chromosom       
-      dat <- Copy(ba9_81.dat)
-      cond1 <- which((dat[,1]==paste0("chr",chrInd)) && (dat[,2] <= wStart && dat[,3] >= wEnd)) ## whole window contained
+      ## read data within a window of a particular chromosom  
+        
+      dat1 <- ba9_81.dat
+      cond1 <- which((dat1[,1]==paste0("chr",chrInd)) && (dat1[,2] <= wStart && dat1[,3] >= wEnd)) ## whole window contained
       #rStart <- if ((dat[,2] - dat[,2]*overlapCutoff) >= 0) (dat[,2] - dat[,2]*overlapCutoff) else 0
       #rEnd <- if ((dat[,3] + dat[,3]*overlapCutoff) <= chrLength) (dat[,3] + dat[,3]*overlapCutoff) else chrLength
       #cond2 <- which((dat[,1]==paste0("chr",chrInd)) && (rStart <= wStart && dat[,3] >= wEnd))   ## window start is before the chr start
       #cond3 <- which((dat[,1]==paste0("chr",chrInd)) && (dat[,2] <= wStart && rEnd >= wEnd))   ## window end is after the chr end
       #temp.ba9_81 <- ba9_81.dat[cond1||cond2 ||cond3,]
-      temp.ba9_81 <- Copy(ba9_81.dat[cond1,])
+      temp.ba9_81 <- ba9_81.dat[cond1,]
+      #part1 <- NULL
+      if(nrow(temp.ba9_81) < 1){  # if nothing found, make an empty row with zeros
+        xx1 <- rep(0,length(temp.ba9_81))
+        nam <- names(temp.ba9_81)
+        #xx1 <- rbind(temp.ba9_81,xx1)
+        temp.ba9_81 <- rbind(temp.ba9_81,xx1)
+        names(temp.ba9_81) <- nam
+      }
+      names(temp.ba9_81) <- paste0(names(temp.ba9_81),".","ba9")
       
-      dat <- Copy(ba41_66.dat)            ## affects the conditions
-      cond1 <- which((dat[,1]==paste0("chr",chrInd)) && (dat[,2] <= wStart && dat[,3] >= wEnd)) ## whole window contained
+      dat2 <- ba41_66.dat            
+      cond2 <- which((dat2[,1]==paste0("chr",chrInd)) && (dat2[,2] <= wStart && dat2[,3] >= wEnd)) ## whole window contained
       #rStart <- if ((dat[,2] - dat[,2]*overlapCutoff) >= 0) (dat[,2] - dat[,2]*overlapCutoff) else 0
       #rEnd <- if ((dat[,3] + dat[,3]*overlapCutoff) <= chrLength) (dat[,3] + dat[,3]*overlapCutoff) else chrLength
       #cond2 <- which((dat[,1]==paste0("chr",chrInd)) && (rStart <= wStart && dat[,3] >= wEnd))   ## window start is before the chr start
       #cond3 <- which((dat[,1]==paste0("chr",chrInd)) && (dat[,2] <= wStart && rEnd >= wEnd))   ## window end is after the chr end
-      #temp.ba41_66 <- ba41_66.dat[cond1|| cond2||cond3,]
-      temp.ba41_66 <- Copy(ba41_66.dat[cond1,])
+      #temp.ba41_66 <- ba41_66.dat[cond1|| cond2||cond3,]      
+      temp.ba41_66 <- ba41_66.dat[cond2,]
+      if(nrow(temp.ba41_66) < 1){  # if nothing found, make an empty row with zeros
+        xx1 <- rep(0,length(temp.ba41_66))
+        #xx1 <- rbind(temp.ba41_66,xx1)
+        nam <- names(temp.ba41_66)
+        temp.ba41_66 <- rbind(temp.ba41_66,xx1)
+        names(temp.ba41_66) <- nam
+        exc <- names(temp.ba41_66) %in% c("chr","start","end")
+        temp.ba41_66 <- temp.ba41_66[!exc]
+      }
+      names(temp.ba41_66) <- paste0(names(temp.ba41_66),".","ba41-42-22")
+       
       
-      dat <- Copy(baVermis.dat)           ## affects the conditions
-      cond1 <- which((dat[,1]==paste0("chr",chrInd)) && (dat[,2] <= wStart && dat[,3] >= wEnd)) ## whole window contained
+      dat3 = baVermis.dat            
+      cond3 <- which((dat3[,1]==paste0("chr",chrInd)) && (dat3[,2] <= wStart && dat3[,3] >= wEnd)) ## whole window contained
       #rStart <- if ((dat[,2] - dat[,2]*overlapCutoff) >= 0) (dat[,2] - dat[,2]*overlapCutoff) else 0
       #rEnd <- if ((dat[,3] + dat[,3]*overlapCutoff) <= chrLength) (dat[,3] + dat[,3]*overlapCutoff) else chrLength
       #cond2 <- which((dat[,1]==paste0("chr",chrInd)) && (rStart <= wStart && dat[,3] >= wEnd))   ## window start is before the chr start
       #cond3 <- which((dat[,1]==paste0("chr",chrInd)) && (dat[,2] <= wStart && rEnd >= wEnd))   ## window end is after the chr end
       #temp.baVermis <- baVermis.dat[cond1||cond2||cond3,]
-      temp.baVermis <- Copy(baVermis.dat[cond1,])
-      
-      names(temp.ba9_81) <- paste0(names(temp.ba9_81),".","ba9")
-      names(temp.ba41_66) <- paste0(names(temp.ba41_66),".","ba41-42-22")
-      names(temp.baVermis) <- paste0(names(temp.baVermis),".","vermis")
-      
-      aRow <- NULL
-      aRow <- cbind(aRow,chrInd, wStart,wEnd)
-      i <- 1
-      while(i <= length(sample.dat$BrainID)) {
-        sam <- paste0(sample.dat$BrainID[i], ".", sample.dat$RegionID[i])     
-        toCheck <- NULL
-        if(sample.dat$RegionID[i] == "ba9"){
-          toCheck <- Copy(temp.ba9_81)
-        }else if(sample.dat$RegionID[i] == "ba41-42-22"){
-          toCheck <- Copy(temp.ba41_66)
-        }else{
-          toCheck <- Copy(temp.baVermis)
-        }
-        val <- if(!is.null(toCheck[[sam]]) && length(toCheck[[sam]]) > 0) toCheck[[sam]] else NA
-        print(paste(sam, toCheck[[sam]], length(toCheck[[sam]])), appendLF=FALSE)
-        aRow <- cbind(aRow,val)
-        ##rm(toCheck)
-        i <- i + 1
+      temp.baVermis <- baVermis.dat[cond3,]
+            
+      if(nrow(temp.baVermis) < 1){  # if nothing found, make an empty row with zeros
+        xx1 <- rep(0,length(temp.baVermis))
+        #xx1 <- rbind(temp.baVermis,xx1)
+        nam <- names(temp.baVermis)
+        temp.baVermis <- rbind(temp.baVermis,xx1)
+        names(temp.baVermis) <- nam
+        
       }
-      ##message(paste(wEnd, ""), appendLF=FALSE)
-      ## bind that row
+      exc <- names(temp.baVermis) %in% c("chr","start","end")
+      temp.baVermis <- temp.baVermis[!exc]
+      names(temp.baVermis) <- paste0(names(temp.baVermis),".","vermis")
+      print(names(temp.baVermis))
+      if(nrow(temp.ba9_81) > 1 || nrow(temp.ba41_66) > 1 || nrow(temp.baVermis) > 1) message("Should not happen !!")      ## because, a bin should be only contained within a chromosomal band (start-end)
+      
+      
+      
+      aRow <- cbind(temp.ba9_81,temp.ba41_66)
+      aRow <- cbind(aRow,temp.baVermis)
+      
       out <- rbind(out,aRow)
       wStart <- wEnd + 1
       binID <- binID + 1
@@ -95,9 +108,9 @@ accetylationDat <- function(ba9_81.filepath, ba41_66.filepath, baVermis_62.filep
     
     chrInd <- chrInd + 1
   }
-  names(out) <- header
+  #names(out) <- header
   #write.csv(out,file = paste(outputPath, "\\outputAccetylation.csv"))
   write.csv(out,file = "outputAccetylation.csv")
 }
 
-accetylationDat(ba9_81.filepath, ba41_66.filepath, ba41_66.filepath, samplefilePath, chrFile, 200, overlapCutoff = 0, outputPath)
+accetylationDat(ba9_81.filepath, ba41_66.filepath, baVermis_62.filepath, samplefilePath, chrFile, 200, overlapCutoff = 0, outputPath)
