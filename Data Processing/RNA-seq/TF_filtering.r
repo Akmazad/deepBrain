b8e3ea5c0205640fa19f0d9c50ab2e29f3a24c7e
Akmazad <a.azad@monash.edu>
2018-12-05 10:01:01 +1100

    Create TF_filtering.r

---
+++ Data Processing/RNA-seq/TF_filtering.r
@@ -0,0 +1,8 @@
+dat <- read.csv("/Volumes/Seagate/STAR_Output/StringTie_all_Transcript_info.csv")
+## download TF gene symbols for human from "http://www.tfcheckpoint.org/data/TFCheckpoint_download_180515.xlsx"
+tf.dat <- read.csv("/Volumes/Seagate/STAR_Output/TFCheckpoint_download_180515.csv")
+tf.human.dat <- tf.dat[which(tf.dat$entrez_human != 0),]
+dbtf.human.dat <- tf.human.dat[which(tf.human.dat$DbTF == 'yes'),]
+
+## filter human tfs only
+dat <- dat[which(dat$gene_name %in% dbtf.human.dat$gene_symbol),]
