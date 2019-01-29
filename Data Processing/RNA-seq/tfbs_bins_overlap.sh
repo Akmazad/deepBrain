28ea059cd7ef721de4e0ef207eca81a9ff6ec388
Akmazad <a.azad@monash.edu>
2019-01-30 10:57:28 +1100

    Create tfbs_bins_overlap.sh

---
+++ Data Processing/RNA-seq/tfbs_bins_overlap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+bedDir=$1
+fileDir=$2
+#cd $fileDir
+bins=$3/$4
+overlapCutof=$(echo $5| bc)
+#fFiles=find . -maxdepth 1 -type f
+#for tfFile in $tfFiles
+for tfFile in "$fileDir"/*.narrowPeak
+do
+  $bedDir/intersectBed -u -f $overlapCutof -a $bins.bed -b $tfFile > $tfFile.overlaps.bed
+  filename="${tfFile##*/}"
+  echo "$filename: [DONE]"
+done
