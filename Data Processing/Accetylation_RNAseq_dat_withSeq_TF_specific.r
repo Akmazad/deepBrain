90dbe38447975795710667ae67280cd252e08c62
Akmazad <a.azad@monash.edu>
2019-02-06 17:54:03 +1100

    Update Accetylation_RNAseq_dat_withSeq_TF_specific.r

--- Data Processing/Accetylation_RNAseq_dat_withSeq_TF_specific.r
+++ Data Processing/Accetylation_RNAseq_dat_withSeq_TF_specific.r
@@ -1,9 +1,17 @@
-
 dataDir="/short/yr31/aa7970/azData/DeepBrain/Data/"
-tfProf=read.csv(paste0(dataDir,"UCSC_Encode_wgEncodeAwgTfbsUniform_metadata_690_TF_profiles.csv"),header=T)[,c(2,6)]
-tfs=read.csv(paste0(dataDir,"tf.genes.expr_0.5_perc.csv"),header=T)
-pos.dat=read.csv(paste0(dataDir,"test.pos.dat"), sep="\t", header=T)
-neg.dat=read.csv(paste0(dataDir,"test.neg.dat"), sep="\t", header=T)
+input.pos.datFile="test.pos.dat"
+input.neg.datFile="test.neg.dat"
+output.pos.datFile="pos.tfSpecific.dat"
+output.neg.datFile="neg.tfSpecific.dat"
+output.combinedFile="pos.neg.tfSpecific.dat"
+tfProfFile="UCSC_Encode_wgEncodeAwgTfbsUniform_metadata_690_TF_profiles.csv"
+selectedTFs="tf.genes.expr_0.5_perc.csv"
+
+library("data.table")
+tfProf=fread(paste0(dataDir,tfProfFile),header=T)[,c(2,6)]
+tfs=fread(paste0(dataDir,selectedTFs),header=T)
+pos.dat=fread(paste0(dataDir,input.pos.datFile), sep="\t", header=T)
+neg.dat=fread(paste0(dataDir,input.neg.datFile), sep="\t", header=T)
 
 processAtf <- function(aTF,dat,tfProf){
   aTF.Prof = paste0(as.vector(tfProf[as.character(tfProf$Factor)==aTF,2]),".narrowPeak.overlaps.bed")
@@ -15,8 +23,12 @@ processAtf <- function(aTF,dat,tfProf){
 }
 new.pos.dat = pos.dat[,c(1:9)]
 new.pos.dat = cbind(new.pos.dat, apply(as.matrix(tfs),1,FUN=function(x) processAtf(x,pos.dat,tfProf)))
-write.csv(new.dat,"pos.cl.csv")
+fwrite(new.pos.dat,paste0(dataDir,output.pos.datFile), row.names=F, quote=F, sep="\t")
 
 new.neg.dat = neg.dat[,c(1:9)]
 new.neg.dat = cbind(new.neg.dat, apply(as.matrix(tfs),1,FUN=function(x) processAtf(x,neg.dat,tfProf)))
-write.csv(new.neg.dat,"neg.cl.csv")
+fwrite(new.neg.dat,paste0(dataDir,output.neg.datFile), row.names=F, quote=F, sep="\t")
+
+#write (pos+neg) combined
+fwrite(rbind(new.pos.dat,new.neg.dat),paste0(dataDir,output.combinedFile), row.names=F, quote=F, sep="\t")
+
