e3d426eae833184f135b16b8d2e694e39b0c4aa3
Akmazad <a.azad@monash.edu>
2019-02-06 17:15:01 +1100

    Update Accetylation_RNAseq_dat_withSeq_TF_specific.r

--- Data Processing/Accetylation_RNAseq_dat_withSeq_TF_specific.r
+++ Data Processing/Accetylation_RNAseq_dat_withSeq_TF_specific.r
@@ -1,7 +1,6 @@
 
 dataDir="/short/yr31/aa7970/azData/DeepBrain/Data/"
 tfProf=read.csv(paste0(dataDir,"UCSC_Encode_wgEncodeAwgTfbsUniform_metadata_690_TF_profiles.csv"),header=T)[,c(2,6)]
-#tfProf[,2]=paste0(tfProf[,2],".narrowPeak.overlaps.bed")
 tfs=read.csv(paste0(dataDir,"tf.genes.expr_0.5_perc.csv"),header=T)
 pos.dat=read.csv(paste0(dataDir,"test.pos.dat"), sep="\t", header=T)
 
@@ -12,7 +11,7 @@ processAtf <- function(aTF,dat,tfProf){
   agg.val =  data.frame( x1 = apply(aTF.Prof.dat[1:ncol(aTF.Prof.dat)], 1, sum))
   colnames(agg.val)=aTF
   agg.val= as.data.frame(ifelse(agg.val>0,1,0))
-  new.dat <<- cbind(new.dat,agg.val) ## assigning new values to the global variable 'new.pos.dat'
+  #new.dat <<- cbind(new.dat,agg.val) ## assigning new values to the global variable 'new.pos.dat'
 }
-apply(as.matrix(tfs),1,FUN=function(x) processAtf(x,pos.dat,tfProf))
-write.csv(colnames(new.dat),"cl.csv")
+new.dat=cbind(new.dat,apply(as.matrix(tfs),1,FUN=function(x) processAtf(x,pos.dat,tfProf)))
+write.csv(new.dat,"cl.csv")
