1d5c150f5c56d2319adee0bb0b75a285dfc86113
Akmazad <a.azad@monash.edu>
2019-02-06 17:22:16 +1100

    Update Accetylation_RNAseq_dat_withSeq_TF_specific.r

--- Data Processing/Accetylation_RNAseq_dat_withSeq_TF_specific.r
+++ Data Processing/Accetylation_RNAseq_dat_withSeq_TF_specific.r
@@ -3,8 +3,8 @@ dataDir="/short/yr31/aa7970/azData/DeepBrain/Data/"
 tfProf=read.csv(paste0(dataDir,"UCSC_Encode_wgEncodeAwgTfbsUniform_metadata_690_TF_profiles.csv"),header=T)[,c(2,6)]
 tfs=read.csv(paste0(dataDir,"tf.genes.expr_0.5_perc.csv"),header=T)
 pos.dat=read.csv(paste0(dataDir,"test.pos.dat"), sep="\t", header=T)
+neg.dat=read.csv(paste0(dataDir,"test.neg.dat"), sep="\t", header=T)
 
-new.dat = pos.dat[,c(1:9)]
 processAtf <- function(aTF,dat,tfProf){
   aTF.Prof = paste0(as.vector(tfProf[as.character(tfProf$Factor)==aTF,2]),".narrowPeak.overlaps.bed")
   aTF.Prof.dat = as.data.frame(dat[,aTF.Prof])
@@ -13,5 +13,10 @@ processAtf <- function(aTF,dat,tfProf){
   agg.val= as.data.frame(ifelse(agg.val>0,1,0))
   #new.dat <<- cbind(new.dat,agg.val) ## assigning new values to the global variable 'new.pos.dat'
 }
-new.dat=cbind(new.dat,apply(as.matrix(tfs),1,FUN=function(x) processAtf(x,pos.dat,tfProf)))
-write.csv(new.dat,"cl.csv")
+new.pos.dat = pos.dat[,c(1:9)]
+new.pos.dat = cbind(new.pos.dat, apply(as.matrix(tfs),1,FUN=function(x) processAtf(x,pos.dat,tfProf)))
+write.csv(new.dat,"pos.cl.csv")
+
+new.neg.dat = neg.dat[,c(1:9)]
+new.neg.dat = cbind(new.neg.dat, apply(as.matrix(tfs),1,FUN=function(x) processAtf(x,neg.dat,tfProf)))
+write.csv(new.neg.dat,"neg.cl.csv")
