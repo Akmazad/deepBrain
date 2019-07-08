b0701edb4446d3d228d61b0f3a5897f3ab4a0a3e
A K M AZAD <a.azad@monash.edu>
2019-07-08 22:09:00 +1000

    Update peak_thresholding.R

--- Data Processing/Accetylation/peak_thresholding.R
+++ Data Processing/Accetylation/peak_thresholding.R
@@ -1,10 +1,11 @@
 # setwd("/Volumes/Data1/PROJECTS/Psychencode_June2019/EpiMap/ProcessedData/")
+rm(list=ls())
 setwd("C:\\Users\\z3526914\\OneDrive - UNSW\\Vafaee Lab\\Projects\\Deep Brain\\Training 2\\")
-library(lattice)
+library(plotrix)
 
 th_select <- function(file, last_val, tfFilter=F){
   denom <- nrow(peaks)
-  val_dist <- seq(1,last_val,1)
+  val_dist <- seq(0,last_val,1)
   perc_dist <- seq(0.1,1,0.05)
   mat <- matrix(data = 0,nrow = length(perc_dist), ncol = length(val_dist))
   i=0
@@ -23,15 +24,16 @@ th_select <- function(file, last_val, tfFilter=F){
 # changes
 fileName <- "mergedPeakHeightMatrix_HumanFC"
 dat = load(paste0(fileName,".rda"))
+peaks[is.na(peaks)] = 0 # replace NA with 0
 hist(peaks,breaks = 200)
 # changes
-last_val <- 20
+# last_val <- 200 # for CNON
+# last_val <- 40 # for YALE
+last_val <- 20 # for BrainGVEX, EpiMap, HumanFC
 
 mat <- th_select(peaks,last_val)
 save(mat,file=paste0(fileName,"_peakTH.rda"))
 
 # Plot
-# pdf(paste0("valTh_VS_percTh_",file,".pdf"))
-par(mar=c(3,4,2,2))
-levelplot(t(mat[c(nrow(mat):1) , ]))
+color2D.matplot(mat,cs1 = c(0,1),cs2 = c(0,1), show.legend = T)
 # dev.off() 
