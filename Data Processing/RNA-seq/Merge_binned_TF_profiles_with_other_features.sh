b37b9b696f575cc24365c20d50b4dda32b5dcc34
A K M AZAD <azad_du28@yahoo.com>
2019-08-16 09:36:13 +1000

    Update Merge_binned_TF_profiles_with_other_features.sh

--- Data Processing/RNA-seq/Merge_binned_TF_profiles_with_other_features.sh
+++ Data Processing/RNA-seq/Merge_binned_TF_profiles_with_other_features.sh
@@ -6,8 +6,8 @@
 #PBS -l mem=1TB
 #PBS -l ncpus=7
 #PBS -P yr31
-#PBS -l walltime=96:00:00
+#PBS -l walltime=02:30:00
 
 module load R/3.5.1
 
-Rscript /short/yr31/aa7970/azData/DeepBrain/Data/Merge_binned_TF_profiles_with_other_features.R
+Rscript /short/yr31/aa7970/azData/DeepBrain/Scripts/Merge_binned_TF_profiles_with_other_features.R
