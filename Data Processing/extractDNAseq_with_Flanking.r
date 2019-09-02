6211c37a3b823e1fb47166c2c55c9817d1aac09f
A K M AZAD <azad_du28@yahoo.com>
2019-09-02 17:39:52 +1000

    Update extractDNAseq_with_Flanking.r

--- Data Processing/extractDNAseq_with_Flanking.r
+++ Data Processing/extractDNAseq_with_Flanking.r
@@ -4,7 +4,6 @@ flankingLength=400
 workingDir = "/Volumes/Data1/PROJECTS/DeepLearning/Test/"
 
 Prep_Acc_RNAseq_dat_withSeq <- function(chrSizeFileName,binSize,flankingLength,workingDir){
-  rm(list=ls())
   # get the total human genome
   library(dplyr)
   library("data.table")
@@ -22,14 +21,14 @@ Prep_Acc_RNAseq_dat_withSeq <- function(chrSizeFileName,binSize,flankingLength,w
   chr_size=chr_size[-grep("_", chr_size$chr, fixed=TRUE),]
   chr_size=chr_size[match(paste0("chr", c(c(1:22), "M", "X", "Y")), chr_size$chr), ]
   
-  ################ generate bed file of bins of size b
+  # generate bed file of bins of size b
   message("Generating bed files for each bins of size b: ",appendLF=F)
   for (j in c(1:nrow(chr_size))){
-    ### these start and end positions doesn't consider initial few bins that don't flanking sequence
+    # these start and end positions doesn't consider initial few bins that don't flanking sequence
     start=seq(from=flankingLength, to=chr_size$size[j]-flankingLength-b, by=b)+1 
     end=seq(from=flankingLength+b, to=chr_size$size[j]-flankingLength, by=b) 
     
-    ## retrieve dna subsequence (with flanking) from hg
+    # retrieve dna subsequence (with flanking) from hg
     chrName=as.character(chr_size$chr[j])
     dnaSeq.start=start-flankingLength
     dnaSeq.end=end+flankingLength
@@ -39,11 +38,12 @@ Prep_Acc_RNAseq_dat_withSeq <- function(chrSizeFileName,binSize,flankingLength,w
     chr_bins=cbind(chrName,start[1:length(end)],end)
     chr_bins=cbind(chr_bins,tempFasta)
     if (j==1) bins=chr_bins else bins=rbind(bins, chr_bins) 
+    print(j)
   }
   bins=as.data.frame(bins)
   colnames(bins)=c("chr", "start", "end", "dna.seq")
   bins$id=paste(bins$chr, bins$start, bins$end, sep="_")
   
-  fwrite(bins,file=paste0(binFile,".bed", sep="\t", row.names=F, quote=F)
+  fwrite(bins,file=paste0(binFile,".fasta.bed"), sep="\t", row.names=F, quote=F)
 
 }
