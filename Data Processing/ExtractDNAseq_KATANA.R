5aa075a22d554f43cb41ef3516789dbdfb5a4542
A K M AZAD <azad_du28@yahoo.com>
2019-10-03 09:10:51 +1000

    Update ExtractDNAseq_KATANA.R

--- Data Processing/ExtractDNAseq_KATANA.R
+++ Data Processing/ExtractDNAseq_KATANA.R
@@ -2,12 +2,23 @@ rm(list = ls(all.names = TRUE))
 setwd('/srv/scratch/z3526914/DeepBrain/Data')
 library(data.table)
 library(dplyr)
-# read binned DNA seq (human)
-hg <- fread("hg19.binwise.fasta.200bp.bed", sep="\t", header=F)
-colnames(hg) <- c("chr","start","end","dna.seq","id","strand")
+
+# ### Option list
+args = commandArgs(trailingOnly=FALSE)
+
+### Make Cluster nodes for parallelizing
+dataDir = args[3]
+flankingLength = args[4]
+inputFile = args[5]
+outputFile = args[5]
+
+library("BSgenome.Hsapiens.UCSC.hg19")
+hg <- BSgenome.Hsapiens.UCSC.hg19
 
 # read non-zero bins
-nonZerobins <- fread("HumanFC_ENCODE_EpiMap_nonZero.binInfo.Union.dat", sep="\t", header=T)
-nonZerobins.seq <- cbind(nonZerobins,hg[which(hg$id %in% nonZerobins$id), "dna.seq"])
+nonZerobins <- fread(inputFile, sep="\t", header=T)
+seq <- getSeq(hg,nonZerobins$chr,start=nonZerobins$start-flankingLength,end=nonZerobins$end+flankingLength)
+seq <- as.character(as.data.frame(seq)[[1]])
+nonZerobins.seq <- cbind(nonZerobins,seq)
 colnames(nonZerobins.seq) <- c(colnames(nonZerobins),"dna.seq")
-fwrite(nonZerobins.seq, file="HumanFC_ENCODE_EpiMap_nonZero.bin.Seq.dat", sep="\t", row.names=F, quote=F)
+fwrite(nonZerobins.seq, file=outputFile, sep="\t", row.names=F, quote=F)
