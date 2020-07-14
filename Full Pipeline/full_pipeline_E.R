331ddc0aa8d1bc26103e77c52abffac95d2c81e7
A K M AZAD <azad_du28@yahoo.com>
2020-07-14 18:35:06 +1000

    Update full_pipeline_E.R

--- Full Pipeline/full_pipeline_E.R
+++ Full Pipeline/full_pipeline_E.R
@@ -16,5 +16,23 @@ source = "EpiMap"
 # ------------- Set Bin-flanking configuration
 binSize = 400
 flanking = 2*binSize     # can be arbitrarily given
-# dataDir = paste0("/srv/scratch/z3526914/DeepBrain/Data/", source, "/",binSize,"_",flanking,"/")
-dir.create(dataDir, recursive=T)
+baseDir = "/srv/scratch/z3526914/DeepBrain/Data/"
+dataDir = paste0(baseDir, source, "/",binSize,"_",flanking,"/")
+dir.create(dataDir, recursive=T) # create the directory if doesn't exists
+
+# ------------- Read random Bins of given size
+bin_outputFile = paste0("hg19_bins_", binSize,"bp","_rand")
+
+# ---------------------------------------------- EpiMap
+# --------------- IntersectBED with bins
+EpiMap_inputFile = "mergedPeakHeightMatrix_EpiMap_filtered"
+# Note: "EpiMap_inputFile" should exist within the "baseDir"
+EpiMap_outputFile = paste0(EpiMap_inputFile,"_randBins.overlaps")
+message("Intersect BED EpiMap:",appendLF=F)
+system2('intersectBed', 
+        paste('-wao -f 0.05 -a', paste0(baseDir,bin_outputFile,".bed"), '-b', paste0(baseDir,EpiMap_inputFile,".bed"), sep=' '), 
+        stdout=paste0(dataDir,EpiMap_outputFile,".bed"), 
+        wait=T)
+message("Done",appendLF=T)
+
+
