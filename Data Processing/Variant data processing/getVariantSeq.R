5469e673a864a9201a054137d91dc3d4924e410a
A K M AZAD <azad_du28@yahoo.com>
2020-07-29 11:50:15 +1000

    Update getVariantSeq.R

--- Data Processing/Variant data processing/getVariantSeq.R
+++ Data Processing/Variant data processing/getVariantSeq.R
@@ -36,34 +36,43 @@ library(data.table)
 library(dplyr)
 
 dir = "/srv/scratch/z3526914/DeepBrain/Data/"
+allSNPFileName = "SNPS"
+codingExonFileName = "CODING.EXONS"
+ncSNPFileName = "NONCODING.SNPS"
+
 # dir = "C:\\Users\\Azad\\OneDrive - UNSW\\Vafaee Lab\\Projects\\Deep Brain\\"
 
 # True Positives (i.e. Functional SNPs - Brain-based)
 filename="brain_specific_eQTL"
 dat = fread(paste0(dir, filename, ".txt")) %>% as.data.frame()
-dat = dat[,c(19,11,20,21,6)]
-colnames(dat) <- c("Chr","Pos","Ref","Alt","Strand")
+dat.sm = dat[,c(10,11,12,9,20,21,6)]     # "SNP_chr"   "SNP_start" "SNP_end"   "SNP_id"    "REF"       "ALT" "strand"
 # find unique snps
-dat = unique(dat)
-fwrite(dat, paste0(dir,"SNPS.BED"), header=F)   # this list has both coding and non-coding SNPs)
+dat.sm = dat.sm[!duplicated(dat.sm[,-c(5,6,7)]),]
+fwrite(dat.sm, paste0(dir,allSNPFileName,".BED"), col.names=F, sep="\t")   # this list has both coding and non-coding SNPs)
+
 message("Intersect BED to get non-coding based SNPs:",appendLF=F)
 system2('intersectBed', 
-        paste('-V -A ', paste0(dir,"SNPS.BED"), '-B', paste0(dir,"CODING.EXONS.BED"), sep=' '), 
-        stdout=paste0(dir,"NONCODING.SNPS.BED"), 
+        paste0('-v -a ', paste0(dir, allSNPFileName,".BED "), ' -b ', paste0(dir,codingExonFileName,".BED ")), 
+        stdout=paste0(dir,ncSNPFileName,".BED"), 
         wait=T)
 message("Done",appendLF=T)
 
-dat$Lable = "1" # all TP
-fwrite(getVariabntSeq(dat, flankingLength), file = paste0(dir, filename, "_fastaseq.csv"), sep = ",")
-
-# True Negative (i.e. non-Functional SNPs - from deepSea supple table 5)
-filename="deepsea_supple_tabale5"
-# anything other than "eQTL" is negative
-dat = as.data.frame(fread(paste0(filename, ".csv"),header = T)) %>% dplyr::filter(label != "eQTL")
-# dat[,8] = ifelse(dat[,8] == "eQTL",1,0)
-dat = dat[,c(2,3,4,5,8)]
-colnames(dat) <- c("Chr","Pos","Ref","Alt","Label")
-dat$Strand = "+"
-
+# read the non
+ncSNPs = fread(paste0(dir,ncSNPFileName,".BED")) %>% as.data.frame() %>% dplyr::select(c(1,2,5,6,7))
+colnames(ncSNPs) = c("Chr", "Pos", "Ref", "Alt", "Strand")
+ncSNPs$Label = "1"
 # invoke function for sequence retreival and save
-fwrite(getVariabntSeq(dat, flankingLength), file = paste0(dir, filename, "_fastaseq_Negatives.csv"), sep = ",")
+fwrite(getVariabntSeq(ncSNPs, flankingLength), file = paste0(dir, filename, "_fastaseq_Positives.csv"), sep = ",")
+
+
+# # True Negative (i.e. non-Functional SNPs - from deepSea supple table 5)
+# filename="deepsea_supple_tabale5"
+# # anything other than "eQTL" is negative
+# dat = as.data.frame(fread(paste0(filename, ".csv"),header = T)) %>% dplyr::filter(label != "eQTL")
+# # dat[,8] = ifelse(dat[,8] == "eQTL",1,0)
+# dat = dat[,c(2,3,4,5,8)]
+# colnames(dat) <- c("Chr","Pos","Ref","Alt","Label")
+# dat$Strand = "+"
+
+# # invoke function for sequence retreival and save
+# fwrite(getVariabntSeq(dat, flankingLength), file = paste0(dir, filename, "_fastaseq_Negatives.csv"), sep = ",")
